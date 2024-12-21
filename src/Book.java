83deb5c3c8a1a4361a79476251d138e4e3230477
robertperse <dgazin@inbox.ru>
2024-12-21 11:09:33 +0300

    ignore

---
+++ src/Book.java
@@ -0,0 +1,30 @@
+public class Book {
+    private String title;
+    private Author author;
+    private int publishingYear;
+
+    public Book(String title, Author author, int publishingYear) {
+        this.title = title;
+        this.author = author;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return  author;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+    public String toString() {
+        return "Книга: \"" + title + "\", Автор: " + author + ", Год публикации: " + publishingYear;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Author author = new Author("Evgeniy", "Nevedov");
+        Author author1 = new Author("Vladimir", "Kuzmin");
 
+        Book book = new Book("Georgia", author, 1938);
+        Book book1 = new Book("Catch me", author1, 1998);
+
+        System.out.println(book);
+        System.out.println(book1);
+
+        book.setPublishingYear(1941);
+        System.out.println(book);
     }
 }
